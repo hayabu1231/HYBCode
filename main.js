a6050219fb1786e4a56afa426c9d7298fc1a1d47
御影　はやぶ <78709561+hayabu1231@users.noreply.github.com>
2024-12-12 00:29:06 +0900

    Hyb improve filepicker UI (#6)
    ファイルピッカーの表示を正常なものに改善

--- main.js
+++ main.js
@@ -152,14 +152,20 @@ function createFileBlock(type, id, name, date, data, service) {
     var info = document.createElement('div');
     var name_item = document.createElement('p');
     name_item.innerText = name;
+    name_item.className = 'files-file-name';
     info.append(name_item);
     if (isFile) {
+        var file_info = document.createElement('div');
+        file_info.className = 'files-file-info';
         var file_date = document.createElement('small');
-        file_date.innerText = date;
-        info.append(file_date);
+        if (date) {
+            file_date.innerText = date;
+        }
+        file_info.append(file_date);
         var file_size = document.createElement('small');
         file_size.innerText = calcDataSize(data);
-        info.append(file_size);
+        file_info.append(file_size);
+        info.append(file_info);
     }
     block.append(info);
     return block;
@@ -221,7 +227,10 @@ function showFilesServices(id) {
         connections.push(service_element);
     });
     services.push(createFileBlock('files', null, 'Files'));
-    document.getElementById('screen-Files-services').replaceChildren(...services);
+    var folder = document.createElement('div');
+    folder.className = 'files-folder';
+    folder.append(...services);
+    document.getElementById('screen-Files-folders').replaceChildren(folder);
     document.getElementById('screen-Connections-list').replaceChildren(...connections);
 }
 function selectFilesService(name, path) {
@@ -288,7 +297,7 @@ function selectFilesService(name, path) {
         folder.replaceChildren(...folders[i]);
         folders[i] = folder;
     }
-    document.getElementById('screen-Files-folders').replaceChildren(...folders);
+    document.getElementById('screen-Files-folders').append(...folders);
 }
 
 //ファイル管理用DB（OPFSが使えるようになったら更新しようね）
